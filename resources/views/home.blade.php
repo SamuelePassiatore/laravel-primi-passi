5eea5153ab4d67588c93c8e55e304e2f9ca27d81
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-22 15:53:05 +0100

    used compact on route $phrase

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
     <title>Home</title>
 </head>
 
 <body>
-    <h1>{{$phrase}}</h1>
+    <div class="text-center h-25 bg-danger py-3">
+        <h1>{{$phrase}}</h1>
+    </div>
+    <header></header>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['phrase' => 'Hello World! I am a new laravel user']);
+    $phrase = 'Hello World! I am a new laravel user';
+    //return view('home', ['phrase' => $phrase]);
+    return view('home', compact('phrase'));
 });
