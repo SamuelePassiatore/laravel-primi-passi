a85c41b17dc00ec7d22487f1ba59b2a976a707cf
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-22 16:18:06 +0100

    added pages array to route and print with foreach blade syntax

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
-    <title>Home</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css'
+        integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=='
+        crossorigin='anonymous' />
+    <title>Test</title>
 </head>
 
 <body>
     <div class="text-center h-25 bg-danger py-3">
-        <h1>{{$phrase}}</h1>
+        <h1>{{ $phrase }}</h1>
     </div>
-    <header></header>
+    <header>
+        <ul>
+            @foreach ($pages as $page)
+                <li>{{ $page }}</li>
+            @endforeach
+        </ul>
+    </header>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $phrase = 'Hello World! I am a new laravel user';
+    $pages = ['Homepage', 'Products', 'Blog', 'Pricing', 'About Us', 'Contacts', 'FAQ'];
     //return view('home', ['phrase' => $phrase]);
-    return view('home', compact('phrase'));
+    return view('home', compact('phrase', 'pages'));
 });
